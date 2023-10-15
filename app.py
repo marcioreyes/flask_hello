9e9ed5eab1afe6d2f014c4de89cd5b6537297cde
Marcio A. M. Reyes <reyes.marcio@gmail.com>
2023-10-15 19:42:03 -0300

    Muitas Alterações

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "ubalubaduba"
 
-@app.route("/hello")
+@app.route("/")
 def index():
     flash("Qual o seu nome?")
     return render_template("index.html")
@@ -12,4 +12,16 @@ def index():
 def greet():
     flash('Olá ' + str(request.form['nome']) + ' prazer em vê-lo!')
     return render_template('index.html')
-
+    
+@app.route("/ranking")
+def ranking():
+    #pegar dados do banco
+    return render_template("ranking.html")
+     
+@app.route("/esqueleto")
+def esqueleto():
+    return render_template("esqueleto.html")
+    
+@app.route("/mobile")
+def mobile():
+    return render_template("mobile.html")
